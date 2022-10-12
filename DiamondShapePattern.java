ae45e79213c0439700b5df84935fe949bb5ddd25
uththaraHansamali <92640862+uththaraHansamali@users.noreply.github.com>
2022-10-12 10:01:38 +0530

    Update DiamondShapePattern.java

--- DiamondShapePattern.java
+++ DiamondShapePattern.java
@@ -1,43 +1,33 @@
+
 package HacktoberFest2022;
-
-
-
+import java.util.Scanner;
 public class Task02 {
   public static void main(String args[]) {
     int row, i, j, space = 1;
-
     System.out.print("Enter the number of rows you want to print: ");
-
-    Scanner sc = new Scan(System.out);
-
-    row = sc.next();
-
+    Scanner scan = new Scanner(System.in);
+    row = scan.nextInt();
     space = row - 1;
-
     for (j = 1; j <= row; j++) {
       for (i = 1; i <= space; i++) {
         System.out.print(" ");
       }
-      space++;
-
-      for (i = 1; i <= 2 * j - 1; i++) {
+      space--;
+      for (a = 1; a <= 2 * j - 1; a++) {
         System.out.print("*");
       }
       System.out.println("");
     }
     space = 1;
-
     for (j = 1; j <= row - 1; j++) {
       for (i = 1; i <= space; i++) {
-        System.out.print(" ")
+        System.out.print(" ");
       }
-
       space++;
-
       for (i = 1; i <= 2 * (row - j) - 1; i++) {
-        System.print("*")
+        System.out.print("*");
       }
-      System.println("");
+      System.out.println("");
     }
   }
 }
